e06d281443a775fa8c4d8b641b1fcad3f080856a
nilspolek <nils@polek.de>
2024-11-05 20:15:18 +0100

    added added a counter

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module test
 
 go 1.23.2
 
-require github.com/nilspolek/Golaxy v0.0.2
+require github.com/nilspolek/Golaxy v0.0.5
--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/nilspolek/Golaxy/router"
+	"github.com/nilspolek/Golaxy/variable"
 )
 
-type Home struct{}
+type Home struct {
+	counter *variable.Integer
+}
+
+func (h *Home) Increase() {
+	for {
+		time.Sleep(time.Millisecond * 100)
+		h.counter.Set(h.counter.Get() + 1)
+	}
+}
 
 func (h Home) Render() string {
-	return "<h1>hello world</h1>"
+	return fmt.Sprintf("<h1>counter: %d</h1>", h.counter.Get())
 }
 
 func main() {
 	r := router.New()
-	r.RegisterRoute("home", Home{})
+	home := Home{counter: variable.NewInt(r)}
+	go home.Increase()
+	r.RegisterRoute("home", home)
 	r.Render()
+	select {}
 }
